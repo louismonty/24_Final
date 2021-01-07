d6f133d7abfd19b70283c6b9ea2a2553f62dbbff
Isak_s205452 <s205452@student.dtu.dk>
2021-01-07 11:06:59 +0100

    Created doubleExtraTurn, getExtraTurn and setExtraTurn method in Rules class

--- src/main/java/Game/Rules.java
+++ src/main/java/Game/Rules.java
@@ -4,6 +4,18 @@ import gui_fields.GUI_Player;
 
 public class Rules
 {
+
+
+    private boolean extraTurn = false;
+
+    public void setExtraTurn(boolean extraTurn) {
+        this.extraTurn = extraTurn;
+    }
+
+    public boolean getExtraTurn() {
+        return extraTurn;
+    }
+
     public void overStartRules(Player currentPlayer,GUIController guiController)
     {
         if(currentPlayer.getPlayerPosition()>=40)
@@ -11,6 +23,15 @@ public class Rules
             currentPlayer.getPlayerAccount().setBalance(currentPlayer.getPlayerAccount().getBalance()+4000);
             currentPlayer.setPlayerPosition(currentPlayer.getPlayerPosition()-40);
             guiController.showMessege("du får 2 m for at komme over start");
+        }
+    }
+
+    public void doubleExtraTurn(Player currentPlayer, DieController dieController)
+    {
+        if(dieController.isDouble() == true)
+        {
+            setExtraTurn(true);
+            currentPlayer.setDoubleCounter(currentPlayer.getDoubleCounter()+1);
         }
     }
 
