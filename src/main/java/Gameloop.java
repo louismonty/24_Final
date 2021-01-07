cf2ac5c7b403536713abbdd5a31fb3fdf21cd37c
Isak_s205452 <s205452@student.dtu.dk>
2021-01-07 09:41:55 +0100

    Created Gameloop class
    Took 2 minutes

---
+++ src/main/java/Gameloop.java
@@ -0,0 +1,18 @@
+
+public class Gameloop
+{
+/*    private DieController dieController = new DieController();
+/    private PlayerController playerController = new PlayerController();
+    private GUIController guiController = new GUIController():
+    private Gameboard gameboard = new Gameboard();
+    private ChanceCardController chanceCardController = new ChanceCardController();
+
+
+    public void matadorGameloop()
+    {
+        private Player[] playerArray;
+        playerController.createPlayer();
+    }
+
+ */
+}
