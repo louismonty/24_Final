861d437a0f45d7b3a927884d65f33cb29dac73a9
Terrell-byte <danieltsutton@outlook.com>
2021-01-07 09:54:08 +0100

    [Changed] [Changed class variables and methods to static] [getName(), setName(), getAccount(), setAccount(), getGUIPlayer(), setGUIPlayer(), getGetOutOfJailCard(), setGetOutOfJailCard(), isIsInJail(), setIsInJail(), getJailCounter(), setJailCounter(), getOwnedFields(), setOwnedFields(), getDoubleCounter(), setDoubleCounter()] in [Player]-class

--- src/main/java/Game/Player.java
+++ src/main/java/Game/Player.java
@@ -9,19 +9,19 @@ import gui_fields.GUI_Player;
 
 public class Player {
 
-    private String name;
-    private Account account;
-    private GUIPlayer GUIPlayer;
-    private int getOutOfJailCard;
-    private boolean isInJail;
-    private int jailCounter;
-    private Fields[] ownedFields;
-    private int doubleCounter;
+    private static String name;
+    private static Account account;
+    private static GUIPlayer GUIPlayer;
+    private static int getOutOfJailCard;
+    private static boolean isInJail;
+    private static int jailCounter;
+    private static Fields[] ownedFields;
+    private static int doubleCounter;
 
     public Player(String name, Account account, GUIPlayer GUIPlayer){
-        this.name = name;
-        this.account = account;
-        this.GUIPlayer = GUIPlayer;
+        Player.name = name;
+        Player.account = account;
+        Player.GUIPlayer = GUIPlayer;
         getOutOfJailCard = 0;
         isInJail = false;
         jailCounter = 0;
@@ -29,67 +29,67 @@ public class Player {
         doubleCounter = 0;
     }
 
-    public String getName() {
+    public static String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public static void setName(String name) {
+        Player.name = name;
     }
 
-    public Account getAccount() {
+    public static Account getAccount() {
         return account;
     }
 
-    public void setAccount(Account account) {
-        this.account = account;
+    public static void setAccount(Account account) {
+        Player.account = account;
     }
 
-    public GUIPlayer getGUIPlayer() {
+    public static GUIPlayer getGUIPlayer() {
         return GUIPlayer;
     }
 
-    public void setGUIPlayer(GUIPlayer GUIPlayer) {
-        this.GUIPlayer = GUIPlayer;
+    public static void setGUIPlayer(GUIPlayer GUIPlayer) {
+        Player.GUIPlayer = GUIPlayer;
     }
 
-    public int getGetOutOfJailCard() {
+    public static int getGetOutOfJailCard() {
         return getOutOfJailCard;
     }
 
-    public void setGetOutOfJailCard(int getOutOfJailCard) {
-        this.getOutOfJailCard = getOutOfJailCard;
+    public static void setGetOutOfJailCard(int getOutOfJailCard) {
+        Player.getOutOfJailCard = getOutOfJailCard;
     }
 
-    public boolean isInJail() {
+    public static boolean isIsInJail() {
         return isInJail;
     }
 
-    public void setInJail(boolean inJail) {
-        isInJail = inJail;
+    public static void setIsInJail(boolean isInJail) {
+        Player.isInJail = isInJail;
     }
 
-    public int getJailCounter() {
+    public static int getJailCounter() {
         return jailCounter;
     }
 
-    public void setJailCounter(int jailCounter) {
-        this.jailCounter = jailCounter;
+    public static void setJailCounter(int jailCounter) {
+        Player.jailCounter = jailCounter;
     }
 
-    public Fields[] getOwnedFields() {
+    public static Fields[] getOwnedFields() {
         return ownedFields;
     }
 
-    public void setOwnedFields(Fields[] ownedFields) {
-        this.ownedFields = ownedFields;
+    public static void setOwnedFields(Fields[] ownedFields) {
+        Player.ownedFields = ownedFields;
     }
 
-    public int getDoubleCounter() {
+    public static int getDoubleCounter() {
         return doubleCounter;
     }
 
-    public void setDoubleCounter(int doubleCounter) {
-        this.doubleCounter = doubleCounter;
+    public static void setDoubleCounter(int doubleCounter) {
+        Player.doubleCounter = doubleCounter;
     }
 }
