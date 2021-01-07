4e5dba3af3938d25ca7e3697f32332787caf5288
Lucas Skals <skalslucas@gmail.com>
2021-01-07 15:26:50 +0100

    Added diceRoll() method and Javadoc comment.

--- src/main/java/Controller/DieController.java
+++ src/main/java/Controller/DieController.java
@@ -1,4 +1,21 @@
 package Controller;
+import Game.Die;
 
-public class DieController {
+
+/**
+ * @author Lucas
+ * Class for DieController that controls the logic for handling Die objects created based on the Die class.
+ */
+
+public class DieController{
+    private Die die1 = new Die();
+    private Die die2 = new Die();
+
+    public void diceRoll(){
+        die1.roll();
+        die2.roll();
+    }
+
+
+
 }
