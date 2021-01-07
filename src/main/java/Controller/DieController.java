aeacb5245d887c4ee4da651ac96c2f55002b0f68
Lucas Skals <skalslucas@gmail.com>
2021-01-07 15:50:27 +0100

    Added diceValue() and isDouble() in DieController class.

--- src/main/java/Controller/DieController.java
+++ src/main/java/Controller/DieController.java
@@ -7,15 +7,26 @@ import Game.Die;
  * Class for DieController that controls the logic for handling Die objects created based on the Die class.
  */
 
-public class DieController{
-    private Die die1 = new Die();
-    private Die die2 = new Die();
+public class DieController {
+    public DieController() {
+    }
 
-    public void diceRoll(){
+    public static Die die1 = new Die();
+    public static Die die2 = new Die();
+
+    public void diceRoll() {
         die1.roll();
         die2.roll();
     }
 
+    public int diceValue() {
+        return die1.getFaceValue() + die2.getFaceValue();
+
+    }
+
+    public static boolean isDouble() {
+        return die1.getFaceValue() == die2.getFaceValue();
+    }
 
 
 }
