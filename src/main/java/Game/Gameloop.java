edca16c8939314a1ec7a3a7a0d463ec830508966
Isak_s205452 <s205452@student.dtu.dk>
2021-01-07 13:23:26 +0100

    added to matadorGameloop method in Gameloop class

--- src/main/java/Game/Gameloop.java
+++ src/main/java/Game/Gameloop.java
@@ -8,11 +8,29 @@ public class Gameloop
     private Gameboard gameboard = new Gameboard();
     private ChanceCardController chanceCardController = new ChanceCardController();
 
-
+    private static boolean isGameRunning = true;
     public void matadorGameloop()
     {
         private Player[] playerArray;
         playerController.createPlayer();
+        while(isGameRunning) {
+
+            currentPlayer = playerController.getCurrentPlayer();
+
+            rules.hasGetOutOfJailCard(fieldController,currentPlayer,playerController.getPlayerArray(),guiController,GuiPlayers[currentPlayer.getPlayerId()],GuiPlayers,chanceCardController);
+
+            rollDice(currentPlayer, GuiPlayers);
+
+            rules.overStartRule(currentPlayer, guiController);
+            //landonField
+            fieldController.getGameboard()[currentPlayer.getPlayerPosition()%24].landOnField(fieldController, currentPlayer, playerController.getPlayerArray(), guiController, GuiPlayers[currentPlayer.getPlayerId()], GuiPlayers, chanceCardController);
+            //updates balance in GUIen
+            GuiPlayers[currentPlayer.getPlayerId()].setBalance(currentPlayer.getPlayerAccount().getBalance());
+            //win
+            isGameRunning = rules.win(currentPlayer, guiController, playerController);
+            //nextPlayer
+        playerController.NextPlayer();
+    }
     }
 
  */
