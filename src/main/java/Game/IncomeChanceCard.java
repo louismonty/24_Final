08bf0ad95ca58f720cea7b0671344a2a2c1c93ea
Isak_s205452 <s205452@student.dtu.dk>
2021-01-08 12:29:13 +0100

    created IncomeChanceCard class with constructor

---
+++ src/main/java/Game/IncomeChanceCard.java
@@ -0,0 +1,14 @@
+package Game;
+
+/**
+ * @author Isak Risager
+ */
+public class IncomeChanceCard extends ChanceCard
+{
+    private int chanceCardID;
+
+    public IncomeChanceCard(int chanceCardID)
+    {
+        super(chanceCardID);
+    }
+}
