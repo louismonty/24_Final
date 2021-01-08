5fb2cab93feda2e98a68cd9874b9a35a74cfe545
louismonty <louis.mail.dk@gmail.com>
2021-01-08 12:54:36 +0100

    add if statement in Menu class
    Took 3 hours 18 minutes

--- src/main/java/Game/Menu.java
+++ src/main/java/Game/Menu.java
@@ -3,8 +3,32 @@ package Game;
 import Controller.GUIController;
 
 public class Menu {
-    public String takeTurnMenu(Language language,GUIController guiController){
-
-        return guiController.buttons(language.getText(0,0),language.getLine(1));
+    public void takeTurnMenu(Language language,GUIController guiController,Player player){
+        while (true) {
+            String playerChose = guiController.buttons(language.getText(0,0), language.getLine(1));
+            if (playerChose == language.getText(1, 0)) {
+                break;
+            }
+            else if((playerChose == language.getText(1, 1))){
+                System.out.println("test2");
+            }
+            else if(playerChose == language.getText(1, 2)) {
+                System.out.println("test2");
+            }
+            else if(playerChose == language.getText(1, 3)){
+                System.out.println("test3");
+            }
+            else if(playerChose == language.getText(1, 4)){
+                System.out.println("test4");
+            }
+        }
     }
-}
+    public void buyHouseOrHotel(Language language,GUIController guiController,Player player){
+        String playerChose = guiController.buttons(language.getText(0, 0));
+
+
+
+    }
+    }
+
+
