80ee0642e50ac09cb0243b8ca3efdcd92dea9d89
Terrell-byte <danieltsutton@outlook.com>
2021-01-08 11:14:50 +0100

    [Created] [Created Field class with variables, constructor, and methods.] [Field(), landOnField(),getName()] in [Field]-class

--- src/main/java/Game/Field.java
+++ src/main/java/Game/Field.java
@@ -1,4 +1,37 @@
 package Game;
 
-public class Field {
+import Controller.GUIController;
+import Controller.PlayerController;
+import gui_fields.GUI_Field;
+import Controller.GameBoard;
+import Controller.ChanceCardController;
+
+import java.awt.*;
+import java.awt.color.*;
+
+/**
+ * @author “Daniel”
+ *
+ */
+
+public abstract class Field {
+    protected int positionID;
+    protected Color colorID;
+    protected String name;
+
+    protected GUI_Field guiField;
+    public Field(int positionID, Color colorID, String name, GUI_Field guiField){
+        this.positionID = positionID;
+        this.colorID = colorID;
+        this.name = name;
+        this.guiField = guiField;
+    }
+
+    public void landOnField(GameBoard gameBoard, ChanceCardController chanceCardController, PlayerController
+            playerController, GUIController guiController){
+
+    }
+    public String getName() {
+        return name;
+    }
 }
