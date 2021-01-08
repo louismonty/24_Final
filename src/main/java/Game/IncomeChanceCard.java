ecedc6ce28ad8f0a4d276aa589638d25443631af
Isak_s205452 <s205452@student.dtu.dk>
2021-01-08 12:37:45 +0100

    created soldFurnitureCard method in IncomeChanceCard class

--- src/main/java/Game/IncomeChanceCard.java
+++ src/main/java/Game/IncomeChanceCard.java
@@ -50,5 +50,11 @@ public class IncomeChanceCard extends ChanceCard
         currentPlayer.getAccount().addBalance(1000);
     }
 
+    public void soldFurnitureCard(Player currentPlayer, GUIController guiController, Language language)
+    {
+        guiController.showChanceCard(language.getText(0,0));
+        currentPlayer.getAccount().addBalance(1000);
+    }
+
 
 }
