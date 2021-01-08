c80592739598110c7a39092d870a66509fe4bcad
Lucas Skals <skalslucas@gmail.com>
2021-01-08 13:40:46 +0100

    Added variables incomeTax and unexpectedTax and methods payIncomeTax and payUnexpectedTax in TaxField.

--- src/main/java/Game/TaxField.java
+++ src/main/java/Game/TaxField.java
@@ -6,6 +6,17 @@ import Controller.*
  * @author Lucas
  * TaxField class that extends Field. Includes two methods: payIncomeTax() and payUnexpectedTax().
  */
+
 public class TaxField extends Field {
+    final private int incomeTax = 4000;
+    final private int unexpectedTax = 2000;
+
+    public void payIncomeTax(Player currentPlayer){
+
+    }
+
+    public void payUnexpectedTax(Player currentPlayer, GUIController guiController){
+
+    }
 
 }
