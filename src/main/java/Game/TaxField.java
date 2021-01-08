f585c4718dccb911a6f738054ddf44ba0e62e182
Lucas Skals <skalslucas@gmail.com>
2021-01-08 12:42:49 +0100

    Updated Javadoc comment in TaxField class

--- src/main/java/Game/TaxField.java
+++ src/main/java/Game/TaxField.java
@@ -1,4 +1,11 @@
 package Game;
 
-public class TaxField {
+import Controller.*
+
+/**
+ * @author Lucas
+ * TaxField class that extends Field. Includes two methods: payIncomeTax() and payUnexpectedTax().
+ */
+public class TaxField extends Field {
+
 }
