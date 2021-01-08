67d7b5740f593ed220e82ea1b07c652fceddb440
Lucas Skals <skalslucas@gmail.com>
2021-01-08 18:29:45 +0100

    Added lanOnField() polymorphic method and getName() in CustomField class.

--- src/main/java/Game/CustomField.java
+++ src/main/java/Game/CustomField.java
@@ -1,8 +1,21 @@
 package Game;
 
-public class CustomField {
+import gui_fields.GUI_Field;
+import java.awt.*;
+import Controller.*;
 
-    public CustomField(){
-        super
+
+public class CustomField extends Field {
+
+    public CustomField(int positionID, String name, GUI_Field guiField, Color color) {
+        super(positionID, color, name, guiField);
+    }
+
+    public void landOnField(GameBoard gameBoard, ChanceCardController chanceCardController, PlayerController playerController, GUIController guiController) {
+
+    }
+
+    public String getName(){
+        return name;
     }
 }
