ccdfcec98c543ba6f935f79bdf2b6ed80a00015e
zitmaster <victorcebula38@gmail.com>
2021-01-08 14:20:08 +0100

    Created if statments in isPairBought and rentSodaMultiplier in SodaField-class

--- src/main/java/Game/SodaField.java
+++ src/main/java/Game/SodaField.java
@@ -1,14 +1,22 @@
 package Game;
+import Controller.DieController;
 
-public class SodaField {
+public class SodaField extends BuyableField {
 
-    public boolean isPairBought()
-    {
+    private int rent = 100;
 
+    public boolean isPairBought() {
+        if(Player.getOwnedFields().contains("Squash","CocaCola")) {
+            return true;
+        }
     }
-    public void rentSodaMultiplier(Player)
-    {
-
+    public void rentSodaMultiplier(DieController dieController) {
+        if(isPairBought()) {
+            rent = (rent*dieController.diceValue())*2;
+        }
+        else {
+            rent = (rent*dieController.diceValue());
+        }
     }
 
 }
