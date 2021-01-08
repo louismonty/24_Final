69625d84a17a79bf05d3eb432720bad4ea969388
Terrell-byte <danieltsutton@outlook.com>
2021-01-08 15:16:28 +0100

    [Created] [GameBoard array] in [GameBoard]-class

--- src/main/java/Controller/GameBoard.java
+++ src/main/java/Controller/GameBoard.java
@@ -1,4 +1,15 @@
 package Controller;
 
+import Game.*;
+
+/**
+ * @author “Daniel”
+ *
+ */
+
 public class GameBoard {
+    private GameBoard gameBoard[] = new GameBoard[39];
+
+    public GameBoard(){
+    }
 }
