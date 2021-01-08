6ed6ebae15fcdb607865a602616601479be789db
Isak_s205452 <s205452@student.dtu.dk>
2021-01-08 12:53:00 +0100

    created JailChanceCard class with constructor

---
+++ src/main/java/Game/JailChanceCard.java
@@ -0,0 +1,16 @@
+package Game;
+
+/**
+ * @Author Isak Risager
+ */
+
+public class JailChanceCard extends ChanceCard
+{
+    private int getChanceCardID;
+
+    public JailChanceCard(int chanceCardID)
+    {
+        super(chanceCardID);
+    }
+
+}
