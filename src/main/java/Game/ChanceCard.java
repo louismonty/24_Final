b7889bdbf4be76e3b07eab32d74becdc716b9a9d
Isak_s205452 <s205452@student.dtu.dk>
2021-01-08 12:10:35 +0100

    Created ChanceCard class with constructor

---
+++ src/main/java/Game/ChanceCard.java
@@ -0,0 +1,11 @@
+package Game;
+
+public abstract class ChanceCard
+{
+    protected int chanceCardID;
+
+    public ChanceCard(int chanceCardID)
+    {
+        this.chanceCardID = chanceCardID;
+    }
+}
