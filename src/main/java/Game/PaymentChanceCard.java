a92414e092889e01c5fde7f9e576f40fe818ef0c
Isak_s205452 <s205452@student.dtu.dk>
2021-01-08 12:23:55 +0100

    created newTiresCard method in PaymentChanceCard class

--- src/main/java/Game/PaymentChanceCard.java
+++ src/main/java/Game/PaymentChanceCard.java
@@ -48,4 +48,10 @@ public class PaymentChanceCard extends ChanceCard
         currentPlayer.getAccount().subtractBalance(3000);
     }
 
+    public void newTiresCard(Player currentPlayer, GUIController guiController, Language language)
+    {
+        guiController.showChanceCard(language.getText(0,0));
+        currentPlayer.getAccount().subtractBalance(1000);
+    }
+
 }
