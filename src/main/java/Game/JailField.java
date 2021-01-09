95f957f5b2dbbc30a2a604bf339c765d19438862
Terrell-byte <danieltsutton@outlook.com>
2021-01-09 08:01:24 +0100

    [Created] [LandOnField] in [JailField]-class

--- src/main/java/Game/JailField.java
+++ src/main/java/Game/JailField.java
@@ -1,5 +1,7 @@
 package Game;
 
+import Controller.GUIController;
+import Controller.GameBoard;
 import gui_fields.GUI_Field;
 import java.awt.*;
 import Controller.PlayerController;
@@ -8,5 +10,14 @@ public class JailField extends Field{
 
     public JailField(int positionID, Color colorID, String name, GUI_Field guiField){
         super(positionID, colorID, name, guiField);
+    }
+    public void goToJail(Player currentPlayer){
+        currentPlayer.setPlayerPosition(30);
+        currentPlayer.isInJail(true);
+    }
+    public void landOnField(GameBoard gameBoard, ChanceCardController chanceCardController, PlayerController
+            playerController, GUIController guiController, Language language){
+        goToJail(playerController.getCurrentPlayer());
+        guiController.showMessage(language.getText(0,0));
     }
 }
