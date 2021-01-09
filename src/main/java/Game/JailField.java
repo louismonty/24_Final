c4961f17a496b467fcc5f8bdb4e5d2022ff0e405
Terrell-byte <danieltsutton@outlook.com>
2021-01-09 07:50:09 +0100

    [Created] [JailField Constructor[JailField] in [JailField]-class

---
+++ src/main/java/Game/JailField.java
@@ -0,0 +1,12 @@
+package Game;
+
+import gui_fields.GUI_Field;
+import java.awt.*;
+import Controller.PlayerController;
+
+public class JailField extends Field{
+
+    public JailField(int positionID, Color colorID, String name, GUI_Field guiField){
+        super(positionID, colorID, name, guiField);
+    }
+}
