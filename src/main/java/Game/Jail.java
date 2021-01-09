6d6707402f4c48b8c7e4e02be98c1b56047f3662
Lucas Skals <skalslucas@gmail.com>
2021-01-09 12:54:28 +0100

    Added payBail method in Jail class.

--- src/main/java/Game/Jail.java
+++ src/main/java/Game/Jail.java
@@ -1,4 +1,19 @@
 package Game;
+import Controller.*;
+
+/**
+ * @author Lucas
+ */
 
 public class Jail {
+    private int bail = 1000;
+
+    /**
+     * @param currentPlayer
+     * A player can choose to pay bail to get out of jail.
+     */
+
+    public void payBail(Player currentPlayer){
+
+    }
 }
