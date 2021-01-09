7b67fc350121e12c99b2577fea9e45b0aab03093
Lucas Skals <skalslucas@gmail.com>
2021-01-09 13:24:49 +0100

    Added conditional statement in rollDiceToGetOut() method in Jail class.

--- src/main/java/Game/Jail.java
+++ src/main/java/Game/Jail.java
@@ -17,15 +17,13 @@ public class Jail {
         currentPlayer.getAccount().subtractBalance(bail);
     }
 
-    public void rollDiceToGetOut(DieController dieController, Player currentPlayer, GUIController guiController,
-                                 ChanceCardController chanceCardController,
-                                 PlayerController playerController, Language language){
+    public void rollDiceToGetOut(DieController dieController, Player currentPlayer,
+                                 Language language){
         dieController.diceRoll();
 
         if(dieController.isDouble()){
             currentPlayer.isInJail(false);
-            System.out.println(language.getText(0,0)); // print "You are out of jail".
+            language.getText(0,0); // print "You are out of jail".
         }
-
     }
 }
