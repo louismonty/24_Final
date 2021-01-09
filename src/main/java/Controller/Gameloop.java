2a42d28fe8aa679a63a1d1136a64075278d21df6
Isak_s205452 <s205452@student.dtu.dk>
2021-01-09 18:28:55 +0100

    made matadorGameloop method in Gameloop class

--- src/main/java/Controller/Gameloop.java
+++ src/main/java/Controller/Gameloop.java
@@ -1,37 +1,54 @@
 package Controller;
+import Game.*;
 
 public class Gameloop
 {
-/*    private DieController dieController = new DieController();
-/    private PlayerController playerController = new PlayerController();
-    private GUIController guiController = new GUIController():
-    private Gameboard gameboard = new Gameboard();
+    private DieController dieController = new DieController();
+    private Language language;
+    private PlayerController playerController = new PlayerController();
+    private GUIController guiController = new GUIController();
+    private GameBoard gameboard = new GameBoard(language);
     private ChanceCardController chanceCardController = new ChanceCardController();
+    private Rules rules = new Rules();
+    private Menu menu = new Menu();
+    private Jail jail = new Jail();
 
     private static boolean isGameRunning = true;
+
     public void matadorGameloop()
     {
-        private Player[] playerArray;
-        playerController.createPlayer();
-        while(isGameRunning) {
+        playerController.createPlayer(language, guiController);
 
-            currentPlayer = playerController.getCurrentPlayer();
-
-            rules.hasGetOutOfJailCard(fieldController,currentPlayer,playerController.getPlayerArray(),guiController,GuiPlayers[currentPlayer.getPlayerId()],GuiPlayers,chanceCardController);
-
-            rollDice(currentPlayer, GuiPlayers);
-
-            rules.overStartRule(currentPlayer, guiController);
-            //landonField
-            fieldController.getGameboard()[currentPlayer.getPlayerPosition()%24].landOnField(fieldController, currentPlayer, playerController.getPlayerArray(), guiController, GuiPlayers[currentPlayer.getPlayerId()], GuiPlayers, chanceCardController);
-            //updates balance in GUIen
-            GuiPlayers[currentPlayer.getPlayerId()].setBalance(currentPlayer.getPlayerAccount().getBalance());
-            //win
-            isGameRunning = rules.win(currentPlayer, guiController, playerController);
-            //nextPlayer
-        playerController.NextPlayer();
+        while(isGameRunning)
+        {
+            Player currentPlayer = playerController.getCurrentPlayer();
+            if(currentPlayer.isInJail(true))
+            {
+                jail.inJail(dieController, currentPlayer, guiController, chanceCardController, language);
+            }
+            else
+                {
+                    menu.takeTurnMenu(language, gameboard, guiController, currentPlayer);
+                    dieController.diceRoll(guiController);
+                }
+            while(true)
+            {
+                gameboard.getGameBoard()[currentPlayer.getPlayerPosition() % 40].landOnField(gameboard, chanceCardController, playerController, guiController, language);
+                rules.overStartRule(currentPlayer, guiController, language);
+                if(currentPlayer.getHasMoved())
+                {
+                    break;
+                }
+                else
+                {
+                    currentPlayer.setHasMoved(false);
+                }
+            }
+            rules.bankrupt(currentPlayer, playerController, guiController);
+            isGameRunning = rules.win(playerController);
+            playerController.nextPlayer();
+        }
     }
-    }
 
- */
+
 }
