1baeebfe2314a3983c0a85898b5e71743c0cb7cf
louismonty <louis.mail.dk@gmail.com>
2021-01-15 10:53:50 +0100

    added plugin for building .jar file and moved gametext to 24_Final folder in pomxml branch
    Took 55 minutes

--- src/main/java/Game/Language.java
+++ src/main/java/Game/Language.java
@@ -12,8 +12,8 @@ public class Language {
         private String[][] importedText = new String[linesInCSV][];
         public void loadFile(){
             try {
-                File CSVFile = new File("src\\main\\GameTextInput.csv");
-                Scanner Reader = new Scanner(CSVFile);
+                File CSVFile = new File("GameTextInput.csv");
+                Scanner Reader = new Scanner(CSVFile,"UTF-8");
                 String line;
                 int i = 0;
                 while(Reader.hasNextLine()) {
